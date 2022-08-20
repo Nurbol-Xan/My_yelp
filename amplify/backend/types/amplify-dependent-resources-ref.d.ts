b52d84f04cf813aa9d551a3c3be785b13f56b14c
Nurbol-Xan <nurbolxaydaraliyev007@gmail.com>
2022-08-20 20:23:20 +0500

    success

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -12,6 +12,7 @@ export type AmplifyDependentResourcesAttributes = {
     },
     "api": {
         "myyelp": {
+            "GraphQLAPIKeyOutput": "string",
             "GraphQLAPIIdOutput": "string",
             "GraphQLAPIEndpointOutput": "string"
         }
