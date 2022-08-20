2eaab366c59c7a86ab4b24ab816c64706275943b
Nurbol-Xan <nurbolxaydaraliyev007@gmail.com>
2022-08-20 19:46:17 +0500

    some error

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -12,7 +12,6 @@ export type AmplifyDependentResourcesAttributes = {
     },
     "api": {
         "myyelp": {
-            "GraphQLAPIKeyOutput": "string",
             "GraphQLAPIIdOutput": "string",
             "GraphQLAPIEndpointOutput": "string"
         }
